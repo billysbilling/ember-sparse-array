5bb3208936b770a1cec58ed7db77a95858f058cf
Sebastian Seilund <sebastian@seilund.com>
2014-03-20 16:25:07 -0700

    added initial code and readme

--- README.md
+++ README.md
@@ -1,1 +1,114 @@
-# ember-sparse-array
+# Ember.js Sparse Array
+
+A sparse array implementation to use in Ember.js projects. It automatically fetches items on-demand.
+
+A common use-case is for extremely long lists, where it's not feasible to load all records into the browser.
+
+
+## Instantiating a SparseArray
+
+When instantiating a `SparseArray`, you have to supply a `load` method. The `load` method is responsible for fetching
+a subset of the resource, e.g. from your server.
+
+The `load` method must accept two arguments:
+
+- `offset`: The 0-based index/offset of the first record to be fetched.
+- `limit`: How many records should be fetched
+
+The `load` method must return a thenable (i.e. a promise or another object with a `.then` method on it) that eventually
+resolves with a hash with two keys:
+
+- `total`: The total number of records.
+- `items`: An array of maximum `limit` items that start at `offset`.
+
+Here is an example:
+
+```javascript
+var SparseArray = require('sparse-array');
+
+var comments = SparseArray.create({
+    load: function(offset, limit) {
+        return new Em.RSVP.Promise(function(resolve) {
+            $.getJSON('/comments?offset='+offset+'&limit='+limit).then(function(payload) {
+                resolve({
+                    total: payload.meta.total,
+                    items: payload.comments
+                });
+            });
+        });
+    }
+});
+```
+
+The `SparseArray` will automatically call your `load` method when items at not-yet-loaded indexes are being requested by
+your application.
+
+
+## Using a SparseArray
+
+The `length` attribute of your `SparseArray` works just like a normal array. It will start out as `0`. It will be set to
+the value of `total` each time a promise from `load` resolves.
+
+`load` is always called instantly with `offset` being `0` when you instantiate a `SparseArray`. This is to find the
+`length` property right away.
+
+The `SparseArray` also has a property called `isLoaded`, which is `false` until the first time a `load` completes,
+where it will be set to `true`, and stay `true` forever.
+
+Calling `.objectAt` at an index that's greater than or equals to the current `length`, will always return `null`. 
+
+Example:
+
+```javascript
+var comments = SparseArray.create({
+    load: function(offset, limit) {/* ... */}
+});
+comments.get('length'); //Will always be 0, since the data hasn't been loaded yet
+comments.get('isLoaded'); //false
+comments.objectAt(0); //null
+comments.objectAt(9999999); //null
+
+//Later, after the first load has completed:
+comments.get('length'); //The `total` value that you resolved the promise with
+comments.get('isLoaded'); //true
+comments.objectAt(0); //Whatever you returned at the index 0
+```
+
+After the `load` method has completed at least once, and the array has a `length` property, requesting an index less
+than `length`, will make the array fetch the items around that index using the `load` method, if the `index` has not 
+been loaded yet.
+
+
+## Notice about using {{each}} and other eager consumers
+
+The `{{each}}` Handlebars helper is "eager", meaning that it will insert views for _all_ items right away. So when your
+load promise resolves with a `total` value of 9000, it will create 9000 views right away, and request each of the 9000 
+items from your array. This may result in _a lot_ of requests.
+
+The real power of sparse arrays is when using them together with container views that only requests (i.e. calls
+`.objectAt`) items that is supposed to be in the browser's current viewport. A good example is
+[Ember.ListView](https://github.com/emberjs/list-view) (disclaimer: This sparse array implementation has not been tested
+with Ember.ListView yet, but you get the idea).
+
+
+## Options
+
+You can set other options than the `load` method when instantiating `SparseArray`. Example:
+
+```javascript
+SparseArray.create({
+    batchSize: 42,
+    load: function() {/* ... */}
+});
+```
+
+The following options are supported:
+
+- `batchSize` (integer): The maximum number of records the sparse array will ask the `load` method to load through the `limit`
+argument. The actual value of `limit` may be lower, since the sparse array will only load items that have not already
+been loaded. Defaults to `100`.
+
+
+## Known limitations
+
+- Rejected promises from the `load` method are currently not handled.
---
+++ tests/sparse-array-test.js
@@ -0,0 +1,343 @@
+var SparseArray = require('../src/sparse-array');
+
+var a,
+    fixtures,
+    loadCalls,
+    willChanges,
+    didChanges;
+
+function fixturesLoad(offset, limit) {
+    loadCalls.push([offset, limit]);
+    return new Em.RSVP.Promise(function(resolve) {
+        resolve({
+            items: fixtures.slice(offset, offset+limit),
+            total: fixtures.length
+        });
+    });
+}
+
+function createSparseArray(batchSize) {
+    a = SparseArray.create({
+        batchSize: batchSize,
+        load: fixturesLoad
+    });
+    a.addArrayObserver(null, {
+        willChange: function(array, start, removed, added) {
+            willChanges.push([start, removed, added]);
+        },
+        didChange: function(array, start, removed, added) {
+            didChanges.push([start, removed, added]);
+        }
+    })
+}
+
+QUnit.module('sparse-array', {
+    setup: function() {
+        fixtures = [];
+        for (var i = 0; i < 100; i++) {
+            fixtures.push(i);
+        }
+
+        loadCalls = [];
+        willChanges = [];
+        didChanges = [];
+    },
+    teardown: function() {
+        a.destroy();
+    }
+});
+
+test('calls load with offset=0 right away', function() {
+    createSparseArray(33);
+    deepEqual(loadCalls, [[0, 33]]);
+});
+
+asyncTest('Sets isLoaded', function() {
+    createSparseArray(2);
+    ok(!a.get('isLoaded'));
+    setTimeout(function() {
+        ok(a.get('isLoaded'));
+        start();
+    }, 0);
+});
+
+asyncTest('Sets length', function() {
+    createSparseArray(2);
+    equal(a.get('length'), 0);
+    setTimeout(function() {
+        equal(a.get('length'), 100);
+        start();
+    }, 0);
+});
+
+test('returns null for indexes out of bounds BEFORE items has been loaded', function() {
+    createSparseArray(2);
+    loadCalls = [];
+    equal(a.objectAt(0), null);
+    equal(a.objectAt(50), null);
+    deepEqual(loadCalls, []);
+});
+
+asyncTest('returns null for indexes out of bounds AFTER items has been loaded', function() {
+    createSparseArray(2);
+    loadCalls = [];
+    setTimeout(function() {
+        equal(a.objectAt(100), null);
+        deepEqual(loadCalls, []);
+        start();
+    }, 0);
+});
+
+asyncTest('returns correct value after it has been loaded', function() {
+    createSparseArray(2);
+    loadCalls = [];
+    setTimeout(function() {
+        equal(a.objectAt(0), 0);
+        deepEqual(loadCalls, []);
+        start();
+    }, 0);
+});
+
+asyncTest('loads more values when requested', function() {
+    createSparseArray(2);
+    loadCalls = [];
+
+    setTimeout(function() {
+        equal(a.objectAt(2).get('isLoaded'), false);
+        deepEqual(loadCalls, [[2, 2]]);
+        loadCalls = [];
+
+        setTimeout(function() {
+            equal(a.objectAt(2), 2);
+            deepEqual(loadCalls, []);
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('optimizes the offset to load', function() {
+    createSparseArray(10);
+    loadCalls = [];
+
+    setTimeout(function() {
+        equal(a.objectAt(12).get('isLoaded'), false);
+        deepEqual(loadCalls, [[10, 10]], 'should load from 10, since up until 9 has already been loaded');
+        loadCalls = [];
+
+        setTimeout(function() {
+            equal(a.objectAt(12), 12);
+            deepEqual(loadCalls, []);
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('loads in the middle of the sparse array', function() {
+    createSparseArray(10);
+    loadCalls = [];
+
+    setTimeout(function() {
+        equal(a.objectAt(47).get('isLoaded'), false);
+        deepEqual(loadCalls, [[42, 10]], 'should load from 42, since batchSize/2 is 5, and 47 - 5 is 42');
+        loadCalls = [];
+
+        setTimeout(function() {
+            equal(a.objectAt(42), 42);
+            deepEqual(loadCalls, []);
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('changes limit when it needs to, in order to load a smaller batch because of already loaded records on each side', function() {
+    createSparseArray(10);
+    loadCalls = [];
+
+    setTimeout(function() {
+        //Get #16, which will trigger a load of #11 through and including #20 (i.e. #10 is not loaded)  
+        equal(a.objectAt(16).get('isLoaded'), false);
+        deepEqual(loadCalls, [[11, 10]]);
+        loadCalls = [];
+
+        setTimeout(function() {
+            //Now get #10  
+            equal(a.objectAt(10).get('isLoaded'), false);
+            deepEqual(loadCalls, [[10, 1]], 'all the items around #10 has already been loaded, so limit should be 1');
+            loadCalls = [];
+
+            setTimeout(function() {
+                equal(a.objectAt(10), 10);
+                deepEqual(loadCalls, []);
+                start();
+            }, 0);
+        }, 0);
+    }, 0);
+});
+
+asyncTest('when length increases', function() {
+    createSparseArray(10);
+    loadCalls = [];
+
+    setTimeout(function() {
+        fixtures.push(100);
+
+        equal(a.objectAt(10).get('isLoaded'), false);
+        deepEqual(loadCalls, [[10, 10]]);
+        equal(a.get('length'), 100);
+        loadCalls = [];
+
+        setTimeout(function() {
+            equal(a.get('length'), 101, 'Length should now have been increased');
+
+            equal(a.objectAt(100).get('isLoaded'), false);
+            deepEqual(loadCalls, [[95, 10]]);
+            loadCalls = [];
+
+            setTimeout(function() {
+                equal(a.objectAt(100), 100);
+                deepEqual(loadCalls, []);
+
+                start();
+            }, 0);
+        }, 0);
+    }, 0);
+});
+
+asyncTest('when length decreases', function() {
+    createSparseArray(10);
+    loadCalls = [];
+
+    setTimeout(function() {
+        fixtures.splice(50, 50);
+
+        equal(a.objectAt(10).get('isLoaded'), false);
+        deepEqual(loadCalls, [[10, 10]]);
+        equal(a.get('length'), 100);
+        loadCalls = [];
+
+        setTimeout(function() {
+            equal(a.get('length'), 50, 'Length should now have been decreased');
+
+            equal(a.objectAt(50), null);
+            deepEqual(loadCalls, []);
+
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('array observers when loading more', function() {
+    createSparseArray(10);
+    deepEqual(willChanges, []);
+    deepEqual(didChanges, []);
+
+    setTimeout(function() {
+        deepEqual(willChanges, [[0, 0, 10], [10, 0, 90]]);
+        deepEqual(didChanges, [[0, 0, 10], [10, 0, 90]]);
+        willChanges = [];
+        didChanges = [];
+
+        a.objectAt(40);
+        deepEqual(willChanges, []);
+        deepEqual(didChanges, []);
+
+        setTimeout(function() {
+            deepEqual(willChanges, [[35, 10, 10]]);
+            deepEqual(didChanges, [[35, 10, 10]]);
+
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('array observers when length increases by loading in the middle', function() {
+    createSparseArray(10);
+
+    setTimeout(function() {
+        willChanges = [];
+        didChanges = [];
+
+        fixtures.pushObjects([100, 101, 102, 103, 104, 105, 106, 107]);
+        //There are 108 items now
+
+        a.objectAt(50);
+        deepEqual(willChanges, []);
+        deepEqual(didChanges, []);
+
+        setTimeout(function() {
+            deepEqual(willChanges, [[45, 10, 10], [100, 0, 8]]);
+            deepEqual(didChanges, [[45, 10, 10], [100, 0, 8]]);
+
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('array observers when length increases by loading at the end', function() {
+    createSparseArray(10);
+
+    setTimeout(function() {
+        willChanges = [];
+        didChanges = [];
+
+        fixtures.pushObjects([100, 101, 102, 103, 104, 105, 106, 107]);
+        //There are 108 items now
+
+        a.objectAt(99);
+        deepEqual(willChanges, []);
+        deepEqual(didChanges, []);
+
+        setTimeout(function() {
+            deepEqual(willChanges, [[94, 6, 10], [104, 0, 4]]);
+            deepEqual(didChanges, [[94, 6, 10], [104, 0, 4]]);
+
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('array observers when length decreases by loading in the middle', function() {
+    createSparseArray(10);
+
+    setTimeout(function() {
+        willChanges = [];
+        didChanges = [];
+
+        fixtures.splice(80, 20);
+        //There are 80 items now
+
+        a.objectAt(50);
+        deepEqual(willChanges, []);
+        deepEqual(didChanges, []);
+
+        setTimeout(function() {
+            deepEqual(willChanges, [[45, 10, 10], [80, 20, 0]]);
+            deepEqual(didChanges, [[45, 10, 10], [80, 20, 0]]);
+
+            start();
+        }, 0);
+    }, 0);
+});
+
+asyncTest('array observers when length decreases by loading at the end', function() {
+    createSparseArray(10);
+
+    setTimeout(function() {
+        willChanges = [];
+        didChanges = [];
+
+        fixtures.splice(80, 20);
+        //There are 80 items now
+
+        a.objectAt(78);
+        deepEqual(willChanges, []);
+        deepEqual(didChanges, []);
+
+        setTimeout(function() {
+            deepEqual(willChanges, [[73, 7, 7], [80, 20, 0]]);
+            deepEqual(didChanges, [[73, 7, 7], [80, 20, 0]]);
+
+            start();
+        }, 0);
+    }, 0);
+});
