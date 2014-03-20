3d4d83a1c523b3a01fd67f5cef927b905bec8276
Sebastian Seilund <sebastian@seilund.com>
2014-03-20 12:58:22 -0700

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        'billy-builder': {
+            title: 'ember-sparse-array',
+            jshint: true
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
